879d7a6d5cb79aa08986979ac6dfc36912164811
Łukasz Janiak <lucasekv2@gmail.com>
2024-03-17 14:12:49 +0100

    Upload tic-tac-toe

---
+++ backend/src/test/java/com/arbuz/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arbuz.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
