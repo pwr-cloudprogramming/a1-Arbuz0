1d2cf8ad70bf656b170658ad2b21ff8c6c5ee52a
Łukasz Janiak <lucasekv2@gmail.com>
2024-06-03 02:35:38 +0200

    Comment config

--- frontend/src/js/cognito-config.js
+++ frontend/src/js/cognito-config.js
@@ -1,3 +1,3 @@
-const COGNITO_DOMAIN = 'mytic-tac-toe.auth.us-east-1.amazoncognito.com';
-const COGNITO_CLIENT_ID = '6jkg57p1l44m5vjshh7nevkkop';
-const COGNITO_REDIRECT_URI = 'http://localhost:8081/login.html';
+// const COGNITO_DOMAIN = 'mytic-tac-toe.auth.us-east-1.amazoncognito.com';
+// const COGNITO_CLIENT_ID = '6jkg57p1l44m5vjshh7nevkkop';
+// const COGNITO_REDIRECT_URI = 'http://localhost:8081/login.html';
